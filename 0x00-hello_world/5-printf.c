9cd7fac12ec3222d3e82a6f2dbab0767c24ec931
Mohamed <mohd.coul@outlook.com>
2023-03-16 21:29:21 +0000

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ *  main - Program print using printF
+ *
+ *   Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (O);
+}
