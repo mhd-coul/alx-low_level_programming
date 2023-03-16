267631c88102de97f0332acbbe6d0e0247d74392
Mohamed <mohd.coul@outlook.com>
2023-03-16 20:30:30 +0000

    taks4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - A programm that print a line with puts F
+* 
+*  Return: Always 0 (Succes)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
+
