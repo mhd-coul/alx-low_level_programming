5fb26958a3af67a8646f3e72be98b2ec2c69b279
Mohamed <mohd.coul@outlook.com>
2023-03-17 18:27:18 +0000

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Task5 print digit number
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+	return (0);
+}
