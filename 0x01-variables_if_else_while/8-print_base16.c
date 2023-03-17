69ec2fd2d4f6af9181305262bb105de30e290000
Mohamed <mohd.coul@outlook.com>
2023-03-17 21:32:38 +0000

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Task8 print all hex num
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	while ( n < 10)
+	{
+		putchar( n + 48);
+		n++;
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
