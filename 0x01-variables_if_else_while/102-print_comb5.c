53ee48172fbfe93169b80ffb301dd7182ff9cfea
Mohamed <mohd.coul@outlook.com>
2023-03-18 17:39:01 +0000

    task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -Task102 program that prints all combo of two two-digit numbers.
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i == 98 && j == 99)
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
