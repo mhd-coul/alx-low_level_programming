7e4335287525a4251020286e232945e3b6846450
Mohamed <mohd.coul@outlook.com>
2023-03-18 12:13:42 +0000

    task101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 
 /**
- * main - Task101 program that prints combinations of three digits.
+ * main -Task101 program that prints three digits comb
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            for (k = j + 1; k < 10; k++)
-            {
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    putchar(i + '0');
-                    putchar(j + '0');
-                    putchar(k + '0');
-                    putchar(',');
-                    putchar(' ');
-                }
-                else
-                {
-                    putchar(i + '0');
-                    putchar(j + '0');
-                    putchar(k + '0');
-                    putchar('\n');
-                }
-            }
-        }
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				if (i != 7 || j != 9 || k != 0)
+				{
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(k + 48);
 
-    return (0);
+					if (i == 7 && j == 8 && k == 9)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+
+	}
+	putchar('\n');
+	return (0);
 }
-
