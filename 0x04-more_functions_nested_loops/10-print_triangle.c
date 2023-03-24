a9a10267fb27935e13b1246abf10591df6ce0c0a
Mohamed <mohd.coul@outlook.com>
2023-03-24 22:50:59 +0000

    task 10 checker issue solved

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,25 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			_putchar(' ');
-		}
-		for (j = size - i - 1; j < size; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(32);
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
