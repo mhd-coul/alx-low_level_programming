90c0b8f32daa71029595fc025f2ebf79a9918c9f
Mohamed <mohd.coul@outlook.com>
2023-03-27 22:13:56 +0000

    task7 solved

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,11 +19,15 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
+		_putchar('\n');
 	}
 	else if (length % 2 != 0)
+	{
 		for (i = ((length - 1) / 2); str[i] != 0; i++)
 		{
 			_putchar(str[i]);
 		}
-	_putchar('\n');
+		_putchar('\n');
+	}
+	
 }
