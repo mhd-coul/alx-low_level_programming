32a6e4fadc8bf8e5043799a997473663cad5ee30
Mohamed <mohd.coul@outlook.com>
2023-03-30 19:26:29 +0000

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverse the content of an array
+ * @a: an array of int
+ * @n: the number of elmt to swap
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str{index} -= 32;
+		index++;
+	}
+	return (str);
+}
