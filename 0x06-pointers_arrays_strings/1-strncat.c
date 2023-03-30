d637e5a5d0190b35e87dfde6cfbaecaafadb256d
Mohamed <mohd.coul@outlook.com>
2023-03-30 17:51:19 +0000

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenate
+ * @dest : input
+ * @src : input
+ * @n : lenght of the input src
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen;
+	int srclen;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		destlen++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		srclen++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[destlen + i] = src[i];
+	}
+
+	return (dest);
+}
