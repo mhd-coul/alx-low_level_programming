f5371addfe6de77a8bac6884974f328a105669be
Mohamed <mohd.coul@outlook.com>
2023-04-07 16:31:06 +0000

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdint.h>
+
+
+/**
+ * main - prints its name
+ * @argc: num of agr passed to the program
+ * @argv: an array of a string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
