284d81b8f6abb97bededa174ec10dc97f49971a0
Mohamed <mohd.coul@outlook.com>
2023-04-18 04:19:22 +0000

    task0 solved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,8 +12,14 @@ struct dog
 {
 	char *name;
 	float age;
-	char *owner
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
