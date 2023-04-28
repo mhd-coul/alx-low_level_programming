3f99ddeac9c8dcf374819560e92012a510aa38d7
Mohamed <mohd.coul@outlook.com>
2023-04-28 20:09:41 +0000

    betty solved 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+/**
+ * before_main - function that prints a
+ * message before the main function is executed
+ *
+ * Description: This function is marked with
+ * the __attribute__ ((constructor)) specifier,
+ * which causes it to be called automatically
+ * before the main function is executed. The
+ * function prints a message to the console
+ * to indicate that it is being called.
+ */
 
 void __attribute__ ((constructor)) before_main()
 {
