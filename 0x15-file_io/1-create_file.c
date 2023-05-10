f99b332a8d10339bcebb6c0dcdbcf1cccacabb45
Mohamed <mohd.coul@outlook.com>
2023-05-10 15:07:59 +0000

    betty solved

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,26 +17,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
+	int fd;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content)
-    {
-        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (text_content)
+	{
+		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
 
-    close(fd);
-    return (1);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
-
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,48 @@
 #include <stdio.h>
 
 /**
+ * print_usage_error - prints an error message for invalid command line usage
+ */
+void print_usage_error(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * print_read_error - prints an error message for failed file read
+ *
+ * @filename: the name of the file that failed to be read
+ */
+void print_read_error(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
+}
+
+/**
+ * print_write_error - prints an error message for failed file write
+ *
+ * @filename: the name of the file that failed to be written
+ */
+void print_write_error(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
+}
+
+/**
+ * print_close_error - prints an error message for failed file descriptor close
+ *
+ * @fd: the file descriptor that failed to be closed
+ */
+void print_close_error(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
  * main - entry point for the cp program
  *
  * @argc: number of arguments to the program
@@ -15,25 +57,18 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		print_usage_error();
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_read_error(argv[1]);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
 			S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
-		exit(99);
+		print_write_error(argv[2]);
 	}
 
 	while ((rd = read(fd_from, buffer, 1024)) > 0)
@@ -41,34 +76,26 @@ int main(int argc, char **argv)
 		wr = write(fd_to, buffer, rd);
 		if (wr == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
+			print_write_error(argv[2]);
 		}
 	}
 
 	if (rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(98);
+		print_read_error(argv[1]);
 	}
 
 	cl = close(fd_from);
 	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		print_close_error(fd_from);
 
 	cl = close(fd_to);
 	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		print_close_error(fd_to);
 
 	return (0);
 }
